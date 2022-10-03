621aca2ad3c83072fa3790ed3dd56ae68d8eaa12
mynenidivya <mynenidivya@localhost>
2022-10-02 23:41:24 -0400

    Actions Commit

--- CRUD operations HR/src/swingpackageUI/CreateJpanel.java
+++ CRUD operations HR/src/swingpackageUI/CreateJpanel.java
@@ -363,7 +363,7 @@ public class CreateJpanel extends javax.swing.JPanel {
            JFileChooser pic = new JFileChooser();
     pic.showOpenDialog(null);
     File f = pic.getSelectedFile();
-    Picture.SetIcon(new ImageIcon(f.toString()));
+   
     FileName=f.getAbsolutePath();
     FilePath.setText(FileName);
         
